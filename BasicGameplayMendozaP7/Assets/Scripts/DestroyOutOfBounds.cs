094121a7bf81486578bf61179ebbb796bf6e94ab
sasham24 <736149@student.dusd.net>
2024-03-18 15:25:45 -0700

    added scripts
    finished game
    challenge 2

--- BasicGameplayMendozaP7/Assets/Scripts/DestroyOutOfBounds.cs
+++ BasicGameplayMendozaP7/Assets/Scripts/DestroyOutOfBounds.cs
@@ -15,12 +15,14 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If an object goes [ast the players view in the game, remove that object
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         } 
         else if (transform.position.z < lowerBound) 
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
--- BasicGameplayMendozaP7/Assets/Scripts/SpawnManager.cs
+++ BasicGameplayMendozaP7/Assets/Scripts/SpawnManager.cs
@@ -8,11 +8,13 @@ public class SpawnManager : MonoBehaviour
     public int animalIndex;
 
     private float spawnRangeX = 16;
-    private float spawnPosZ = 16; 
+    private float spawnPosZ = 16;
+    private float startDeley = 2;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnrandomAnimal", startDeley, spawnInterval);
     }
 
     // Update is called once per frame
@@ -20,11 +22,14 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-
-            int animalIndex = Random.Range(0, animalPrefabs.Length); 
-
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
+    }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+       
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
