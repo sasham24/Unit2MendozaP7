b7a2e892a57f86112c894cbfce638497943371be
sasham24 <736149@student.dusd.net>
2024-03-22 15:22:50 -0700

    FwdgyuksdVGHJKasf ghjvasdfjb

--- BasicGameplayMendozaP7/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ BasicGameplayMendozaP7/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -26,7 +26,8 @@ public class SpawnManagerX : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        int ballNumber = Random.Range(0, ballPrefabs.Length);
+        Instantiate(ballPrefabs[ballNumber], spawnPos, ballPrefabs[ballNumber].transform.rotation);
     }
 
 }
